de74b7650bbf4841bf8126532915e3bab0919a78
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-04-22 14:17:14 +0800

    Add phpmyadmin, mailcatcher, selenium support.

--- runtime/coder/run.d/401-setup_php.sh
+++ runtime/coder/run.d/401-setup_php.sh
@@ -7,4 +7,7 @@ set -o pipefail
 
 run "cp ${PHP_INI_DIR}/php.ini-production ${PHP_INI_DIR}/php.ini" "${DEBUG_LEVEL}"
 run "cp /run.d/uploads.conf ${PHP_INI_DIR}/conf.d/uploads.ini" "${DEBUG_LEVEL}"
-run "cp /run.d/xdebug.ini ${PHP_INI_DIR}/conf.d/zz-xdebug.ini" "${DEBUG_LEVEL}"
+
+if [ "$EN_XDEBUG" = "true" ]; then
+	run "cp /run.d/xdebug.ini ${PHP_INI_DIR}/conf.d/zz-xdebug.ini" "${DEBUG_LEVEL}"
+fi
