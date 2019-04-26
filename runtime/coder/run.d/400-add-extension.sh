0ff2ce16779c69c2d1ae8afea3eba8d33ac59413
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-04-26 12:18:13 +0800

    Add GitLens extension.

--- runtime/coder/run.d/400-add-extension.sh
+++ runtime/coder/run.d/400-add-extension.sh
@@ -14,8 +14,8 @@ if ! [ -f add_extension.done ]; then
     add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/felixfbecker/vsextensions/php-debug/1.13.0/vspackage" "${DEBUG_LEVEL}"
     # Setup React Extension
     add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vsmobile/vsextensions/vscode-react-native/0.9.1/vspackage" "${DEBUG_LEVEL}"
-    # Setup Mysql Extension
-    #add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/formulahendry/vsextensions/vscode-mysql/0.4.0/vspackage" "${DEBUG_LEVEL}"
+    # Setup GitLen Extension
+    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/eamodio/vsextensions/gitlens/9.6.3/vspackage" "${DEBUG_LEVEL}"
 
     touch add_extension.done
 fi
