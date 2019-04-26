2fd3af17fca1139a6b218d5bdf6f737cf4f4528a
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-04-26 11:59:41 +0800

    Update README.md, Update disable xdebug setting.

--- runtime/coder/run.d/401-setup_php.sh
+++ runtime/coder/run.d/401-setup_php.sh
@@ -10,4 +10,6 @@ run "cp /setting/uploads.conf ${PHP_INI_DIR}/conf.d/uploads.ini" "${DEBUG_LEVEL}
 
 if [ "$EN_XDEBUG" = "true" ]; then
 	run "cp /setting/xdebug.ini ${PHP_INI_DIR}/conf.d/zz-xdebug.ini" "${DEBUG_LEVEL}"
+else
+	sed -i "s/^zend/;zend/g" ${PHP_INI_DIR}/conf.d/docker-php-ext-xdebug.ini
 fi
