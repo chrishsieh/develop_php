c49b2edb59d7dc5e9a449c6c2e391478fc746330
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-05-09 14:13:15 +0800

    Reduce coder image size, Fix extension path issue. Add scss rewrite rule.

--- runtime/coder/run.d/400-add-extension.sh
+++ runtime/coder/run.d/400-add-extension.sh
@@ -9,13 +9,13 @@ if ! [ -f add_extension.done ]; then
     ### Add extensions
     ###
     # Setup PHP IntelliSense Extension
-    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/felixfbecker/vsextensions/php-intellisense/2.3.10/vspackage" "${DEBUG_LEVEL}"
+    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/felixfbecker/vsextensions/php-intellisense/latest/vspackage" "${DEBUG_LEVEL}"
     # Setup PHP Debug Extension
-    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/felixfbecker/vsextensions/php-debug/1.13.0/vspackage" "${DEBUG_LEVEL}"
+    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/felixfbecker/vsextensions/php-debug/latest/vspackage" "${DEBUG_LEVEL}"
     # Setup React Extension
-    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vsmobile/vsextensions/vscode-react-native/0.9.1/vspackage" "${DEBUG_LEVEL}"
+    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/msjsdiag/vsextensions/vscode-react-native/latest/vspackage" "${DEBUG_LEVEL}"
     # Setup GitLen Extension
-    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/eamodio/vsextensions/gitlens/9.6.3/vspackage" "${DEBUG_LEVEL}"
+    add_extension "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/eamodio/vsextensions/gitlens/latest/vspackage" "${DEBUG_LEVEL}"
 
     touch add_extension.done
 fi
