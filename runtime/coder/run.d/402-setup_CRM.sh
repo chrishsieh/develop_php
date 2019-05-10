571b97a1983d687037a73cf13c1f510d2cc4c078
chrishsieh <902006+chrishsieh@users.noreply.github.com>
2019-05-10 12:31:09 +0800

    Clean build cache, change php-fpm uid to coder, update php-fpm setting.

--- runtime/coder/run.d/402-setup_CRM.sh
+++ runtime/coder/run.d/402-setup_CRM.sh
@@ -20,11 +20,11 @@ if ! [ -f git_clone.done ]; then
     run "chmod +x ./scripts/*.sh" "${DEBUG_LEVEL}"
 
     cd src
-    run "find . -type d -exec chmod -R 775 {} \;" "${DEBUG_LEVEL}"
-    run "find . -type f -exec chmod -R 664 {} \;" "${DEBUG_LEVEL}"
+    run "find . -type d -exec chmod -R 755 {} \;" "${DEBUG_LEVEL}"
+    run "find . -type f -exec chmod -R 644 {} \;" "${DEBUG_LEVEL}"
 
     cd ../..
-    run "chown -R coder:www-data CRM" "${DEBUG_LEVEL}"
+    run "chown -R coder:coder CRM" "${DEBUG_LEVEL}"
     touch git_clone.done
 fi
 
